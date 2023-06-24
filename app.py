02498706e48c61d90c47fa12d050da252ced35d4
Swarup Sapkota <empswarupcoc@gmail.com>
2023-06-24 10:48:43 +0545

    added upto login

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 # import flask module
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
+import pymysql
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world(  ):
+@app.route("/",methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        a_email = request.form["email"]
+        a_password = request.form["password"]
+        print(a_email,a_password)
+        
+        print("Connecting to database")
+        conn = pymysql.connect(
+            host='localhost',
+            user="root",
+            password="",
+            db='elective' 
+        )
+  
+        cur = conn.cursor()
+        query = "SELECT * FROM users WHERE username='%s' and password='%s';" % (a_email,a_password)
+        cur.execute(query)
+        output = cur.fetchall()
+        print(output)
+        conn.close()
+        if len(output) > 0:
+            return redirect('/home')
+
     return render_template("login.html")
   
 @app.route("/register")
